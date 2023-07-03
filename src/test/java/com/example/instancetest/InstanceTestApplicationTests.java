61890abf1d69abc1641d18a0587352f39b35fa63
koesnam <76582376+10kseok@users.noreply.github.com>
2023-07-03 15:41:59 +0900

    configuration

---
+++ src/test/java/com/example/instancetest/InstanceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.instancetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstanceTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
