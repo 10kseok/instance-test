2c0589467159b9bf03c49c9b9f978a1d8b63d49a
koesnam <76582376+10kseok@users.noreply.github.com>
2023-07-03 15:49:12 +0900

    say hello api

---
+++ src/main/java/com/example/instancetest/TestRestController.java
@@ -0,0 +1,18 @@
+package com.example.instancetest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * {설명을 작성해주세요}
+ *
+ * @author koesnam
+ * @since 2023/07/03
+ */
+@RestController
+public class TestRestController {
+    @GetMapping
+    public String sayHello() {
+        return "hello";
+    }
+}
